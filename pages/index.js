09dc002f00fd1ab170835f9d522cd02fc0719b72
balasabi <balas@kenlasystems.com>
2022-02-05 21:56:32 +0530

    changed

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Balasubramani
+         Bala
         </h1>
         <p className={styles.description}>
           My project
