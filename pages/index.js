8eaddb1aa676d363cc0fcd34251482fa7a100764
balasabi <balas@kenlasystems.com>
2022-02-15 21:04:31 +0530

    code clean

--- pages/index.js
+++ pages/index.js
@@ -60,6 +60,7 @@ const rows = [
   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 10, lastName: 'A', firstName: 'Raja', age: 65 },
 ];
 
 
